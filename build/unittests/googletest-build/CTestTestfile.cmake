93a0bbbf9e50fad9731512579eb2cdd8c283404c
JETM319 <jetmeans@gmail.com>
2020-05-04 18:30:22 -0400

    Remove build dir

--- build/unittests/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/ALL CODING PROGRAMS/ALL C++ Programs in VS Code/katas/build/unittests/googletest-src
-# Build directory: D:/ALL CODING PROGRAMS/ALL C++ Programs in VS Code/katas/build/unittests/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/unittests/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/ALL CODING PROGRAMS/ALL C++ Programs in VS Code/katas/build/unittests/googletest-src/googlemock
-# Build directory: D:/ALL CODING PROGRAMS/ALL C++ Programs in VS Code/katas/build/unittests/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
